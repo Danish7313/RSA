52039ecd6d73a024f32a9182a714ceb5a17d0b1b
nursyahiraamira <60992106+nursyahiraamira@users.noreply.github.com>
2020-03-14 23:12:58 +0800

    FIXED CHANGE PASSWORD ----> MAKE SURE DELETE ALL YOUR EMAIL IN FIREBASE, TRY SIGN UP AGAIN, THEN BARU TRY CHANGE PASSWORD

--- LoginDemo 230220/app/src/main/java/com/example/logindemo/UpdatePassword.java
+++ LoginDemo 230220/app/src/main/java/com/example/logindemo/UpdatePassword.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UpdatePassword extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class UpdatePassword extends AppCompatActivity {
     private EditText newPassword;
     private FirebaseUser firebaseUser;
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,8 @@ public class UpdatePassword extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +49,13 @@ public class UpdatePassword extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(UpdatePassword.this, "Password Changed", Toast.LENGTH_SHORT).show();
                             finish();
-                        }else {
-                            Toast.makeText(UpdatePassword.this, "Password Not Changed", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(UpdatePassword.this, "Password Update Failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
-
 
     }
 
